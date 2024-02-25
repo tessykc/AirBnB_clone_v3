be17a839ae2023d36a5c267a6165cc8295fc1175
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-25 20:27:43 +0300

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -35,3 +35,71 @@ def amenity_by_place(place_id):
                  strict_slashes=False)
 def unlink_amenity_from_place(place_id, amenity_id):
   """
+  unlink an amenity in place
+  :param place_id: place id
+  :param amenity_id: amenity id
+  :return: empty dict or error
+  """
+  if not storage.get("Place", str(place_id)):
+    abort(404)
+  if not storage.get("Amenity", str(amenity_id)):
+    abort(404)
+
+  fetched_obj = storage.get("Place", (place_id)
+  found = 0
+
+  for obj in fetched_obj.amenities:
+    if str(obj.id) == obj.amenity_id:
+      if getenv("HBNB_TYPE_STORAGE") == "db":
+        fetched_obj.amenities.remove(obj)
+      else:
+        fetched_obj.amenity_ids.remove(obj)
+      fetched_obj.save()
+      found = 1
+      break
+
+  if found == 0:
+    abort(404)
+  else:
+    resp = jsonify({})
+    resp.status_code = 201
+    return resp
+
+
+@app_views.route("places/<place_id>/amenities",
+                 methods=["POST"],
+                 strict_slashes=False)
+def link_amenity_to_place(place_id, amenity_id):
+  """
+  link an amenity in place
+  :param place_id: place id
+  :param amenity_id: amenity id
+  :return: returns amenity obj added or error
+  """
+
+  fetched_obj = storage.get("Place", str(place_id))
+  amenity_obj = storage.get("Amenity", str(amenity_id))
+  found_amenity = None
+
+  if not fetched_obj or not amenity_obj:
+    abort(404)
+
+  for obj in fetched_obj.amenities:
+    if str(obj.id) == amenity_id:
+      found_amenity = obj
+      break
+
+  if found_amenity is not None:
+    return jsonify(found_amenity.to_json())
+
+  if getenv("HBNB_TYPE_STORAGE") == "db":
+    fetched_obj.amenities.append(ammenity_obj)
+  else:
+    fetched_obj.amenities = amenity_obj
+
+  fetched_obj.save()
+
+  resp = jsonify(amenity_obj.to_json())
+  resp.status_code = 201
+
+  return resp
