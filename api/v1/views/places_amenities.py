33e5562d636de57309d50bac93012b8769a4c8ad
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-25 19:34:55 +0300

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""
+Route for handling places and amenities linking.
+"""
+from flask import jsonify, abort
+from os import getenv
+
+from api.v1.views import app_views, storage
 
