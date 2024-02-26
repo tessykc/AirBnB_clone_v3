0e559d871ad463dbedfd484bb4b3761628000abc
tessykc <tessykc@gmail.com>
2024-02-26 02:22:24 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 """app.py to connect to API"""
 import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
-=======
+
 """
 A variable app
 """
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-
-from api.v1.views import app_views
-from models import storage
->>>>>>> master
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-<<<<<<< HEAD
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
@@ -39,7 +28,7 @@ def page_not_found(error):
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
-=======
+
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
@@ -66,4 +55,3 @@ def handle_404(exception):
 
     if __name__ == "__main__":
         app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
->>>>>>> master
