802597aa2b604b1f20493a21a6a1914baccf2fc3
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-26 21:38:10 +0300

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,6 +5,7 @@ Routes for handling city objects and operations.
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.city import City
+from models.state import State
 
 
 @app_views.route("/states/<state_id>/cities", methods=["GET"], strict_slashes=False)
@@ -13,14 +14,11 @@ def city_by_state(state_id):
     Retrieves all city objects from specific states
     :Returns: json of all cities in a state or 404 error
     """
-    city_list = []
     state = storage.get("State", state_id)
-    
-    if state_obj is None:
+    if not state:
         abort(404)
-    for obj in state_obj.cities:
-        city_list.append(obj.to_json())
-        
+
+    city_list = [city.to_dict() for city in state.cities]
     return jsonify(city_list)
 
 
@@ -43,16 +41,15 @@ def create_city(state_id):
 
     city_json["state_id"] = state_id
 
-    new_city = City(**city_json)
+    new_city = City(**city_json)  # Added missing colon here
     new_city.save()
-    resp = jsonify(new_city.to_json())
+    resp = jsonify(new_city.to_dict())
     resp.status_code = 201
 
     return resp
 
 
-@app_views.route("/cities/<city_id>", methods=["GET"],
-                 strict_slashes=False)
+@app_views.route("/cities/<city_id>", methods=["GET"], strict_slashes=False)
 def city_by_id(city_id):
     """
     Gets specific City object by ID
@@ -60,16 +57,15 @@ def city_by_id(city_id):
     :Returns: city obj with the specified id or error
     """
 
-    fetched_obj = storage.get("City", str(city_id))
+    fetched_obj = storage.get("City", city_id)
 
     if fetched_obj is None:
         abort(404)
 
-    return jsonify(fetched_obj.to_json())
+    return jsonify(fetched_obj.to_dict())
 
 
-@app_views.route("/cities/<city_id>", methods=["PUT"],
-                 strict_slashes=False)
+@app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
 def city_put(city_id):
     """
     Updates specific city object ID
@@ -86,7 +82,7 @@ def city_put(city_id):
         if key not in ["id", "created_at", "updated_at", "state_id"]:
             setattr(fetched_obj, key, val)
     fetched_obj.save()
-    return jsonify(fetched_obj.to_json())
+    return jsonify(fetched_obj.to_dict())
 
 
 @app_views.route("/cities/<city_id>", methods=["DELETE"], strict_slashes=False)
@@ -97,7 +93,7 @@ def city_delete_by_id(city_id):
     :Returns: Empty dict with 200 or 404 not found
     """
 
-    fetched_obj = storage.get("City", str(city_id))
+    fetched_obj = storage.get("City", city_id)
 
     if fetched_obj is None:
         abort(404)
