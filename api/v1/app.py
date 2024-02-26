a296f01a248c1874bb05e5cfc5aab6cc45eff5dd
tessykc <tessykc@gmail.com>
2024-02-26 12:48:55 +0100

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
 """app.py to connect to API"""
-import os
-#from models import storage
-from os import getenv
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-#from api.v1.views import app_views
+from models import storage
+from os import getenv
+from api.v1.views import app_views
 """
 A variable app
 """
 app = Flask(__name__)
-#app.register_blueprint(app_views)
+app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
-@app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
-    storage.close()
+#@app.teardown_appcontext
+#def teardown_appcontext(code):
+#    """teardown_appcontext"""
+#    storage.close()
 
 
 @app.teardown_appcontext
