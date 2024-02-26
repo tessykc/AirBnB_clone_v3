2a9313cff1a242ec9a3a85428027e7f0683ca657
tessykc <tessykc@gmail.com>
2024-02-26 03:07:44 +0100

    Init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,23 +10,10 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 """of everything in the index.py module"""
 from api.v1.views.index import *
-"""from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
-"""
-
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
-
-
-from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.users import *
 from api.v1.views.places_amenities import *
