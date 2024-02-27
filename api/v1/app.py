74640853e42cb249d0011643392aacb75b5303c5
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-27 10:50:21 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
 
-from api.v1.views import app_views
+#from api.v1.views import app_views
 from models import storage
 """
 A variable app
