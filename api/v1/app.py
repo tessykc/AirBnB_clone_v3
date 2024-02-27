5a1cef75ca793d9b2eef731ea566b2f6adbff588
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-27 10:52:44 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""
+app.py to connect to API
+"""
 
-"""app.py to connect to API"""
 from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
 
-#from api.v1.views import app_views
+from api.v1.views import app_views
 from models import storage
 """
 A variable app
