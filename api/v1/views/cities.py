0da62c6fd22db981a089e2bf21998e4671e6df28
tessykc <tessykc@gmail.com>
2024-02-29 11:55:27 +0100

    City

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,6 +12,7 @@ from models import storage, State, City
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def get_cities(state_id):
+    """Retrieves a City object"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -21,6 +22,7 @@ def get_cities(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
+    """Retrieves a City object"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -30,6 +32,7 @@ def get_city(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
+    """Creates a City object"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -47,6 +50,7 @@ def create_city(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
+    """Updates a City object"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -62,6 +66,7 @@ def update_city(city_id):
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
+    """Deletes a City object"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
